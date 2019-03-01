fa9202007e5902d545683a9177fdf0b64deb2411
marco ravazzini <marcoravazzini@marcos-MacBook-Pro.local>
2019-03-01 18:15:25 +0100

    add parsers

--- DataParser/main.py
+++ DataParser/main.py
@@ -1,8 +1,17 @@
-from parser_lib import Parser
-
+import os
+import re
 IS_DEBUG = True
 
 
-p = Parser('./data/backup/')
-a= p.mi_sleep_parser( number_of_files=0, isDebug=IS_DEBUG )
-print(a)
+path = './data/dump_2019-03-01-15-05-31.log'
+
+
+with open(path, "r") as in_file:
+    for line in in_file:
+        total_time = re.search(r'totalTime=\"(\d\d:\d\d)\"', line)
+        name = re.search(r'package=(?!com\.google\.android\.|com\.android|org.lineageos|com\.qualcomm|com\.quicinc).*\.(.*) t', line)
+
+        if name and total_time:
+            print(name.group(1), total_time.group(1))
+        if 'ChooserCounts' in line:
+            break
--- DataParser/parser_lib.py
+++ DataParser/parser_lib.py
@@ -1,11 +1,13 @@
-import pandas as pd
-from pandas.io.json import json_normalize
 import json
 import os.path
 from enum import Enum
 import glob
+import re
+import datetime
+import pandas as pd
+from pandas.io.json import json_normalize
+import numpy as np
 
-# import numpy as np
 # import matplotlib.pyplot as plt
 
 
@@ -14,15 +16,36 @@ class Type(Enum):
     NUMBER = 2
 
 
+def haversine_np(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.    
+
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(
+        dlat / 2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km
+
+
 class Parser:
     def __init__(self, miband_basepath, location_basepath):
         self.miband_basepath = miband_basepath
-        self.location_basepath = location_basepath
+        self.location_basepath = '/Users/marcoravazzini/RavazDrive/Config_files/Android_OP3_Sync/Gps_Logger/'
 
     def __mi_generic_parser__(self, files_path, pickle_path, main_key,
                               number_of_files, columns):
         main_df = ''
-        if not os.path.isfile(pickle_path) or isDebug:
+        if not os.path.isfile(pickle_path) or is_debug:
             for a in range(0, number_of_files + 1):
                 a = a if number_of_files > 0 else ''
                 curr_path = files_path.format(a)
@@ -51,7 +74,7 @@ class Parser:
 
         return main_df
 
-    def mi_heart_parser(self, number_of_files, isDebug=False):
+    def mi_heart_parser(self, number_of_files, is_debug=False):
         '''Return a DataFrame with the date as index and heart rate value as instensity column'''
 
         files_path = os.path.join(self.miband_basepath, 'logReportHeart{}.bak')
@@ -61,7 +84,7 @@ class Parser:
                                           'HeartMonitorData', number_of_files,
                                           columns)
 
-    def mi_steps_parser(self, number_of_files, isDebug=False):
+    def mi_steps_parser(self, number_of_files, is_debug=False):
         '''Return a DataFrame with the date as index and cumulative number of steps in steps column'''
 
         files_path = os.path.join(self.miband_basepath, 'logReportSteps{}.bak')
@@ -70,7 +93,7 @@ class Parser:
         return self.__mi_generic_parser__(files_path, pickle_path, 'StepsData',
                                           number_of_files, columns)
 
-    def mi_sleep_parser(self, number_of_files, isDebug=False):
+    def mi_sleep_parser(self, number_of_files, is_debug=False):
         '''
             Return a DataFrame with the date as index, the total mitntes of sleep,
             the minutes of REM and NON REM sleep and the starting and ending
@@ -88,23 +111,67 @@ class Parser:
         return self.__mi_generic_parser__(
             files_path, pickle_path, 'SleepDayData', number_of_files, columns)
 
-    def location_parser(self, isDebug=False):
-        files = glob.glob(
-            "/Users/marcoravazzini/RavazDrive/Config_files/Android_OP3_Sync/Gps_Logger/*.csv"
-        )
-     
+    def location_parser(self, is_debug=False):
+        '''Parse GpsLogger files'''
+
+        files = glob.glob(os.path.join(self.location_basepath, '*.csv'))
+
         main_df = ''
         for file in files:
             df = pd.read_csv(
-                file,
-                usecols=['time', 'lat', 'lon', 'elevation', 'battery'])
+                file, usecols=['time', 'lat', 'lon', 'elevation', 'battery'])
             df['time'] = pd.to_datetime(df.time)
             if main_df is '':
                 main_df = df
             else:
                 main_df = pd.concat([main_df, df])
         main_df.set_index('time', inplace=True)
+
+        # print( main_df.iloc[1:, 1])
+        main_df['dist'] = haversine_np(
+            main_df.lon.shift(), main_df.lat.shift(), main_df.iloc[1:, 1],
+            main_df.iloc[1:, 0])
+        print(main_df.to_string())
+
         return main_df
-        
+
+    def app_usage_parser(self):
+        path = '/Users/marcoravazzini/RavazDrive/Config_files/Android_OP3_Sync/App_Usage/'
+        files = glob.glob(os.path.join(path, 'app_usage_*.log'))
+
+        df = pd.DataFrame(columns=['time', 'app_name', 'usage'])
+        counter = 0
+        for file in files:
+            with open(file, "r") as in_file:
+                for line in in_file:
+                    total_time = re.search(r'totalTime=\"(.*)\" l', line)
+                    name = re.search(
+                        r'package=(?!com\.google\.android\.|com\.android|org.lineageos|com\.qualcomm|com\.quicinc).*\.(.*) t',
+                        line)
+
+                    if name and total_time:
+
+                        usage = total_time.group(1) if len(total_time.group(1)) > 5 else '00:'+total_time.group(1)
+                        df.loc[counter] = [datetime.datetime.now(), name.group(1), usage]
+                        counter +=1
+                    if 'ChooserCounts' in line:
+                        break
+        df['usage'] = pd.to_timedelta(df.usage)
+        return df
+
+    def battery_parser(self):
+        path = '/Users/marcoravazzini/RavazDrive/Config_files/Android_OP3_Sync/App_Usage/'
+        files = glob.glob(os.path.join(path, 'battery*.csv'))
+        main_df = ''
+        for file in files:
+            df = pd.read_csv(file, skipinitialspace=True)
+            if main_df is '':
+                main_df = df
+            else:
+                main_df = pd.concat([main_df, df], axis=0)
+
+        main_df['time'] = pd.to_datetime(main_df.time, unit='ms')
+        main_df.set_index('time', inplace=True)
+        return main_df
 
 
